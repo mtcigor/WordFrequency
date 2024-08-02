11aae69e3ac1ddc20357572427b2207840671e57
mtcigor <igormtc05@gmail.com>
2024-08-02 16:54:03 +0100

    LoadFile and CountAllWords functions rework

--- Files.h
+++ Files.h
@@ -1,8 +1,20 @@
+/**
+ * @igormtc
+ * July 2024
+ * Header file for all hash functions and data structures
+ */
+
 #ifndef FILES_H
 #define FILES_H
 
 #include "WordFrequency.h"
 
+#define BUFFER 256
+
 FileCounted* LoadFile(const char* filename, bool* error);
+
+void RemovePunctuation(char* word);
+void ToLowercase(char *word);
+int CountAllWords(const char* filename);
 
 #endif //FILES_H
